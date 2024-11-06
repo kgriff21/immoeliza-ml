3831df2f52daf566cd33f66041573bd82c4ad4d0
Kelli Griffin <kelligriffin49@gmail.com>
2024-11-06 17:50:57 +0100

    Made more functions and saved LR model with joblib

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import joblib
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import OneHotEncoder
@@ -11,14 +12,13 @@ from sklearn.ensemble import RandomForestRegressor
 from IPython.display import display
 
 def encode_building_condition(df):
-    '''
+    """
     Encodes building condition with OrdinalEncoder, giving hierarcy ranging from 0 - 5:
     ['To restore', 'To renovate', 'To be done up', 'Good', 'Just renovated', 'As new']
     This function also drops the column originally titled 'Building Condition'.
     Parameters:
     df (DataFrame): The original or modified DataFrame
-    '''
-
+    """
     hierarchy = ['To restore', 'To renovate', 'To be done up', 'Good', 'Just renovated', 'As new']
     building_condition_column = df[['Building condition']]
     # Define and fit the OrdinalEncoder
@@ -27,19 +27,15 @@ def encode_building_condition(df):
     # Add the transformed data back to the DataFrame
     df['Encoded Building Condition'] = encoded_building_condition.ravel()
     df = df.drop(['Building condition'], axis=1)
-    # Display the updated DataFrame
-    print(display(df.head(50)))
     return df
 
 
 def one_hot_encode_columns(df, columns_to_encode):
     """
     One-hot encode the specified columns in the DataFrame.
-    Parameters:
-    df (DataFrame): The original DataFrame
-    columns_to_encode (list): List of columns to be one-hot encoded
-    Returns:
-    DataFrame: The DataFrame with the specified columns one-hot encoded and added.
+    :param df (DataFrame): The original DataFrame
+    :param columns_to_encode (list): List of columns to be one-hot encoded
+    :return DataFrame: The DataFrame with the specified columns one-hot encoded and added.
     """
     for column in columns_to_encode:
         column_data = df[[column]]
@@ -48,14 +44,43 @@ def one_hot_encode_columns(df, columns_to_encode):
         encoded_df = pd.DataFrame(encoded_data, columns=encoder.get_feature_names_out([column]))
         df = pd.concat([df, encoded_df], axis=1)
         df = df.drop(column, axis=1)
+    # print(df['Property type'])
     return df  # return outside the loop to process all columns before returning updated DataFrame
 
+def drop_irrelevant_columns(df, columns_to_drop):
+    """ Drop unnecessary columns not needed for the model.
+    :param df: dataframe with encoded columns
+    :param columns_to_drop: Columns with unnecessary data or Nans to be left out of df for model training
+    :return: new df with specified columns dropped
+    """
+    clean_df = df.drop(columns_to_drop, axis=1)
+    return clean_df
+
+def preprocess_data(df):
+    """Processes a dataframe including drop irrelevant columns, encode building conditions and column encoding
+     ('Property type', 'Province').
+     :param : original df to process
+    :return : cleaned dataframe
+     """
+    # Drop unnecessary columns not needed for the model
+    df = drop_irrelevant_columns(df, ['Property ID', 'Open fire', 'Unnamed: 0', 'Locality data', 'Region',
+                                      'Price per m²',
+                                      'Terrace surface m²', 'Garden area m²'])
+    # Encode building condition
+    df = encode_building_condition(df)
+    # Call one_hot_encode_columns() function
+    columns_to_encode = ['Property type', 'Province']
+    df_clean = one_hot_encode_columns(df, columns_to_encode)  # update the df on each iteration
+    print(display(df_clean['Property type_house'].head()))
+    print(display(df_clean.head(10)))
+    return df_clean
+
 def split_dataset(df):
-    '''
+    """
     Split the dataset into training and test sets.
     :param: dataframe
     :return: A tuple of variables for X_train, X_test, y_train, y_test that will be used for model training
-    '''
+    """
     y = df['Price'] # Target variable
     X = df.drop(columns=['Price']) # All other column features in df after dropping the target
     X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=41, test_size=0.2) # Get 4 parameters back
@@ -81,21 +106,25 @@ def standardize_data_features(df):
     return X_train_standardized, X_test_standardized, y_train_standardized, y_test_standardized
 
 def fit_train_linear_regression(X_train, X_test, y_train, y_test):
+    """ Trains a linear regression model on the provided preprocessed dataframe and saves the model to a file.
+    :param X_train (pandas.DataFrame): Training set
+    :param X_test (pandas.DataFrame): Test set
+    :param y_train (pandas.Series): Training target (price) variable
+    :param y_test (pandas.Series): Testing target variable
+    :return:
+    """
     # Fit linear regression model
     lr = LinearRegression()
     # Finds the best fit line on the training model (find m (slope) and c (coefficient, y-intercept)) on training parameters
-    lr.fit(X_train, y_train)
+    trained_model = lr.fit(X_train, y_train)
     c = lr.intercept_ # View the fitted and calculated y-intercept
     m = lr.coef_ # View the fitted and calculated slope value
-    # print(m, c)
     # Train model and predict scores on test dataset
     train_score = lr.score(X_train, y_train)
     test_score = lr.score(X_test, y_test)
     print(f"Coefficient of determination (R^2) for trained data: {train_score}")
     print(f"Coefficient of determination (R^2) for test data: {test_score}")
     y_pred = lr.predict(X_test) # Generate predictions from a fitted liner regression model on new unseen data.
-    # print(y_test)
-    # print(y_pred)
     lr.score(X_test, y_test)
     # Calculate Mean Squared Error and R^2 Score from sklearn.metrics import
     mse = mean_squared_error(y_test, y_pred)
@@ -104,24 +133,19 @@ def fit_train_linear_regression(X_train, X_test, y_train, y_test):
     r2 = r2_score(y_test, y_pred)
     print("Linear regression Mean Squared Error:", mse.round(2))
     print("Linear regression R^2 Score:", round(r2, 2))
+    # Save the trained model using joblib
+    joblib.dump(trained_model, 'models/trained_lr_model.joblib')
+    return trained_model
 
 
 df = pd.read_csv("Data/cleaned_data_with_region_and_price_per_m2.csv")
 
-# Encode building condition
-df = encode_building_condition(df)
-
-# Drop unnecessary columns not needed for the model
-df = df.drop(['Property ID', 'Open fire', 'Unnamed: 0', 'Locality data', 'Region', 'Price per m²', 'Terrace surface m²', 'Garden area m²'], axis=1)
-
-# Call one_hot_encode_columns() function
-columns_to_encode = ['Property type', 'Province']
-df = one_hot_encode_columns(df, columns_to_encode) # update the df on each iteration
+df_clean = preprocess_data(df)
 
 # Split the dataset: Call the function and assign the returned values to variables
-X_train, X_test, y_train, y_test = split_dataset(df)
+X_train, X_test, y_train, y_test = split_dataset(df_clean)
 
-X_train_standardized, X_test_standardized, y_train_standardized, y_test_standardized = standardize_data_features(df)
+# X_train_standardized, X_test_standardized, y_train_standardized, y_test_standardized = standardize_data_features(df)
 
 # Commented out standardized train and test data as did not make an impact on score.
 # fit_train_linear_regression(X_train_standardized, X_test_standardized, y_train_standardized, y_test_standardized)
@@ -146,6 +170,6 @@ mse_forest = mean_squared_error(y_test, y_pred)
 root_mse = np.sqrt(mse_forest)
 print("Random Forest Regression Mean Squared Error:", mse_forest.round(2))
 print(f'Random Forest Regression Root Mean Squared Error: {root_mse.round(2)}')
-correlation_matrix = df.corr(method='pearson')
+correlation_matrix = df_clean.corr(method='pearson')
 price_correlation = correlation_matrix['Price'].sort_values(ascending=False)
 print(price_correlation)
